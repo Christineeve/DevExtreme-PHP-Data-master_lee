465115edd7282fb7dd0e13502e7a1271c7f4faf6
Christine Lee <Christineeve@Hotmail.com>
2019-08-20 20:58:47 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,95 @@
+﻿$(function() {
+    //var url = "C:/xampp/htdocs/dashboard/DevExtreme-PHP-Data-master/example/php/service.php";
+	var url = "../example/php/service.php";
+    var db = DevExpress.data.AspNet.createStore({
+        key: "ID",
+        loadUrl: url,
+        insertUrl: url,
+        updateUrl: url,
+        deleteUrl: url
+    });
+    $("#gridContainer").dxDataGrid({
+        dataSource: { 
+            store: db
+        },
+        height: "100%",
+        columns: [{
+            dataField: "ID",
+            dataType: "number",
+            allowEditing: false
+        }, {
+            dataField: "Name"
+        }, {
+            dataField: "Category"
+        }, {
+            dataField: "CustomerName"
+        }, {
+            dataField: "BDate",
+            dataType: "date"
+        }],
+        groupPanel: {
+            visible: true
+        },
+        sorting: {
+            mode: "multiple"
+        },
+        searchPanel: {
+            visible: true
+        },
+        scrolling: {
+            mode: "virtual"
+        },
+        filterRow: {
+            visible: true
+        },
+        editing: {
+            mode: "batch",
+            allowAdding: true,
+            allowUpdating: true,
+            allowDeleting: true
+        },
+        grouping: {
+            autoExpandAll: false
+        },
+        pager: {
+            showPageSizeSelector: true,
+            showInfo: true
+        },
+        summary: {
+            totalItems: [{
+                column: "ID",
+                summaryType: "sum"
+            },
+            {
+                column: "ID",
+                summaryType: "avg"
+            }, 
+            {
+                column: "ID", 
+                summaryType: "min"
+            }, {
+                column: "ID", 
+                summaryType: "max"
+            }],
+            groupItems: [{
+                summaryType: "count"
+            },{
+                column: "ID", 
+                summaryType: "min"
+            }, {
+                column: "ID", 
+                summaryType: "max"}]
+            },
+            remoteOperations: {
+                filtering: true,
+                grouping: true,
+                groupPaging: true,
+                paging: true,
+                sorting: true,
+                summary: true  
+            },
+            headerFilter: {
+                visible: true
+            }
+    });
+});
